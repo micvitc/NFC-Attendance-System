b2ff4d92795d238b1ada3d1d6be1598314bda554
Tejaswini <tejaswinithota83@gmail.com~>
2023-06-14 19:12:41 +0530

    flask initialised

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify 
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow 
+from flask import Flask, request
+
+
+app= Flask(__name__)
+marsh = Marshmallow(app)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nfc.sqlite3' 
+db = SQLAlchemy(app) 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+
+class Meeting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('posts', lazy=True)) 
+
+class Attendance(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('posts', lazy=True)) 
+
+@app.route('/create_tables')
+def create_tables():
+    db.create_all()
+    return 'Tables created successfully!'
+
+if __name__ == '__main__':
+    app.run()
