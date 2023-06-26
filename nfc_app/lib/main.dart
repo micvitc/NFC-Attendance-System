f6a7af6a783e43b112e0998c42755d270eb07e8f
Shivansh Tariyal <shivanshtariyal@gmail.com>
2023-06-26 18:47:20 +0530

    NFC Scanner

--- nfc_app/lib/main.dart
+++ nfc_app/lib/main.dart
@@ -1,16 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:nfc_app/views/home.dart';
+import 'package:nfc_manager/nfc_manager.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'NFC Attendance System',
-      home: Home(),
+      title: 'NFC Attendence',
+      home: MyHomePage(title: 'NFC Attendence App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _nfcData = '';
+
+  void _scanNFC() async {
+    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+      setState(() {
+        _nfcData = tag.data.toString();
+      });
+      NfcManager.instance.stopSession();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'NFC Data:',
+            ),
+            Text(
+              '$_nfcData',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _scanNFC,
+        tooltip: 'Scan NFC',
+        child: Icon(Icons.nfc),
+      ),
     );
   }
 }
